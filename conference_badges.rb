ba5e9227d811f2573924dc880b2eb3efcfafbca5
Dan-Vy Le <danvyle85@gmail.com>
2019-05-15 00:33:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,14 +21,24 @@ def assign_rooms(speakers)
 # array.each_index {|x| puts "Hello, #{array[x]}! You'll be assigned to room #{x + 1}" }
 room_number = 1 
 room_messages = []
-  speakers.each do |speakers|
-  room_messages << "Hello, #{speakers}! You'll be assigned to room #{room_number}!"
+  speakers.each do |speaker|
+  room_messages << "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
   room_number += 1
   end
   room_messages
 end
 
-
+# def assign_rooms(speakers)
+#   # assigns each speaker to a room, rooms 1-7.
+#   # return a list of room assignments in the form of: "Hello____! You'll be assigned to room ___!"
+#   room_number = 1
+#   room_messages = []
+#   speakers.each do |speaker|
+#     room_messages << "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+#     room_number += 1
+#   end
+#   room_messages
+# end
 
 
 # def printer
