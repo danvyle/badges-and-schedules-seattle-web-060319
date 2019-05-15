2da16f55072ba8b5f95c1b6fd485db917e99ca34
Dan-Vy Le <danvyle85@gmail.com>
2019-05-15 00:29:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,17 @@ def batch_badge_creator(name)
 badge_names = []
   badge_names.each do |name|
   message = badge_maker(name)
-  badge_messages << message
+  badge_names << message
+  end
+  badge_names
+end
+
+def batch_badge_creator(speakers)
+  # returns an array of badge messages, using badge_maker
+  badge_messages = []
+  speakers.each do |speaker|
+    message = badge_maker(speaker)
+    badge_messages << message
   end
   badge_messages
 end
