68f1fdcdeb67df59a677db999df8832916944132
Dan-Vy Le <danvyle85@gmail.com>
2019-05-15 00:34:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -28,17 +28,7 @@ room_messages = []
   room_messages
 end
 
-# def assign_rooms(speakers)
-#   # assigns each speaker to a room, rooms 1-7.
-#   # return a list of room assignments in the form of: "Hello____! You'll be assigned to room ___!"
-#   room_number = 1
-#   room_messages = []
-#   speakers.each do |speaker|
-#     room_messages << "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
-#     room_number += 1
-#   end
-#   room_messages
-# end
+
 
 
 # def printer
